655863c8032f023c92e00fec4d10e79a85364c69
JH_Lee <53180127+hello6135@users.noreply.github.com>
2026-02-18 23:59:27 +0900

    [chore] aws s3 버전 v3로 변경

--- src/main/java/com/finance/finportfolio/global/config/WebConfig.java
+++ src/main/java/com/finance/finportfolio/global/config/WebConfig.java
@@ -6,7 +6,6 @@ import java.nio.file.Paths;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
