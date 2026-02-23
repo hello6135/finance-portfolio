4d68214339729444ed04ab0bbe871ce1fa98a116
JH_Lee <53180127+hello6135@users.noreply.github.com>
2026-02-23 21:59:17 +0900

    [fix] S3Properties 빈 등록 누락

--- src/main/java/com/finance/finportfolio/infrastructure/file/S3Properties.java
+++ src/main/java/com/finance/finportfolio/infrastructure/file/S3Properties.java
@@ -1,9 +1,12 @@
 package com.finance.finportfolio.infrastructure.file;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
 import lombok.NonNull;
 
-@ConfigurationProperties
+@Configuration
+@ConfigurationProperties(prefix = "cloud.aws.s3")
 public record S3Properties(
         @NonNull String bucketName,
         @NonNull String cloudfrontDomain) {
