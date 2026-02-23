11eee646012283663a19581c8c8531cc4504c027
JH_Lee <53180127+hello6135@users.noreply.github.com>
2026-02-23 22:39:15 +0900

    [fix] local 환경 복구

--- src/main/java/com/finance/finportfolio/infrastructure/file/S3Properties.java
+++ src/main/java/com/finance/finportfolio/infrastructure/file/S3Properties.java
@@ -2,9 +2,11 @@ package com.finance.finportfolio.infrastructure.file;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import lombok.NonNull;
 
+@Profile("dev")
 @ConfigurationProperties(prefix = "cloud.aws.s3")
 public record S3Properties(
         @NonNull String bucketName,
