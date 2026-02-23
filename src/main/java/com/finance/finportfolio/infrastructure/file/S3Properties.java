6eb8f46d8dbc2751834cd7b3d958fd6fae8cdc99
JH_Lee <53180127+hello6135@users.noreply.github.com>
2026-02-23 23:57:34 +0900

    [fix] 미참조 이미지 삭제 pattern 수정

--- src/main/java/com/finance/finportfolio/infrastructure/file/S3Properties.java
+++ src/main/java/com/finance/finportfolio/infrastructure/file/S3Properties.java
@@ -1,9 +1,6 @@
 package com.finance.finportfolio.infrastructure.file;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
 import lombok.NonNull;
 
 @ConfigurationProperties(prefix = "spring.cloud.aws.s3")
