8c7d0ecef384bf80aea6387e746111ecd7dc2d66
JH_Lee <53180127+hello6135@users.noreply.github.com>
2026-02-24 22:07:48 +0900

    [Refactor] 이미지 저장 인프라 단일화 및 로컬 저장 로직(Legacy) 폐기

--- src/main/java/com/finance/finportfolio/global/config/S3Config.java
+++ src/main/java/com/finance/finportfolio/global/config/S3Config.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+@Profile("local | dev | prod")
 public class S3Config {
+    // AWS 인증 정보 처리 - Access key, Secret Key
+    // S3Client 빈 생성 - 실제 파일 업로드/삭제
+    // 보안 및 권한 부여 방식 결정
 }
