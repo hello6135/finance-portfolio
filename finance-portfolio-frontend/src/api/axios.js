aa6e0ca9ca0b196bd639488ce2bf240c0b93e0a9
JH_Lee <53180127+hello6135@users.noreply.github.com>
2026-03-14 00:53:49 +0900

    [Fix] axios 쿠기 추가

--- finance-portfolio-frontend/src/api/axios.js
+++ finance-portfolio-frontend/src/api/axios.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+axios.defaults.withCredentials = true; // 쿠키 공유 허용
+axios.defaults.xsrfCookieName = 'XSRF-TOKEN'; // 쿠키에서 읽을 이름
+axios.defaults.xsrfHeaderName = 'X-XSRF-TOKEN'; // 헤더에 넣을 이름
+
 // 1. axios 인스턴스 생성
 const axiosInstance = axios.create({
     // 환경 변수에서 기본 주소를 가져옵니다.
