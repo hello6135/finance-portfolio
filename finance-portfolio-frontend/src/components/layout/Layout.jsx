cc4381968fcee83384c062b4b3a2d97b17e9676c
JH_Lee <53180127+hello6135@users.noreply.github.com>
2026-04-08 23:30:47 +0900

    [Refactor] 프론트 UI 추가 개선

--- finance-portfolio-frontend/src/components/layout/Layout.jsx
+++ finance-portfolio-frontend/src/components/layout/Layout.jsx
@@ -5,7 +5,6 @@ import Navbar from './Navbar';
 const Layout = () => {
     return (
         <div className="d-flex flex-column bg-theme-custom"
-            // nav 크기 56px
             style={{ minHeight: 'calc(100vh - 56px)' }}>
             <Navbar />
             <main className="flex-grow-1 d-flex flex-column">
